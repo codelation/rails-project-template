c7733b7adde08d34b69a72069d15ee3ccc18a3be
Brian Pattison <brian@brianpattison.com>
2015-11-16 12:05:05 -0600

    Force no flash for live reload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.generators.assets = false
-  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload, no_swf: true
 end
 Rails.application.routes.default_url_options[:host] = "localhost:3000"
 
