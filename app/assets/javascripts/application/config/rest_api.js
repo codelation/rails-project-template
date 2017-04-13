4ff9c1d8c455e1ae532f04f521f3fc29f38c7601
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 11:20:44 -0500

    Update rest_api.js

--- app/assets/javascripts/application/config/rest_api.js
+++ app/assets/javascripts/application/config/rest_api.js
@@ -3,8 +3,6 @@
 
   // Rest API Config,
   // see https://github.com/codelation/codelation_ui/blob/master/app/assets/javascripts/codelation_ui/std/interfaces/rest_api.js#L5
-  App.vue.config.rest_api = {
-    'version': 1
-  }
+  App.vue.config.rest_api.version = 1;
 
 })();
