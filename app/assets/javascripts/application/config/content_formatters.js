767c5a9f7e973dd51b6009df8eda808d3ed26761
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 11:22:47 -0500

    Update content_formatters.js

--- app/assets/javascripts/application/config/content_formatters.js
+++ app/assets/javascripts/application/config/content_formatters.js
@@ -3,13 +3,11 @@
 
   // Content Formatters Config,
   // see https://github.com/codelation/codelation_ui/blob/master/app/assets/javascripts/codelation_ui/vue_pages.js#L7
-
-  // App.vue.config.contentFormatters = {
-  //   'empty': '--',
-  //   'currency': '0,0[.]00',
-  //   'percent': '0,0[.]00',
-  //   'number': '0,0[.]00',
-  //   'date': 'MM/DD/YYYY',
-  // };
+  
+  // App.vue.config.contentFormatters.empty = '--';
+  // App.vue.config.contentFormatters.currency = '0,0[.]00';
+  // App.vue.config.contentFormatters.percent = '0,0[.]00';
+  // App.vue.config.contentFormatters.number = '0,0[.]00';
+  // App.vue.config.contentFormatters.date = 'MM/DD/YYYY';
 
 })();
