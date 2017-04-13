b79ccdf1eb33067d2f0f7733eb6e151b55d0ced8
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 12:43:27 -0500

    Update main.js

--- app/assets/javascripts/application/config/main.js
+++ app/assets/javascripts/application/config/main.js
@@ -5,7 +5,10 @@
 
     // Displays all available UI options in the warnings
     App.vue.config.main.showInterfaces = true;
-
+    
+    // Sets what the default root vue component should attach to
+    App.vue.config.main.rootComponentNode = 'body';
+    
     // Interfaces included into every vue component
     App.vue.config.main.includedInterfaces = [
       App.vue.interfaces.contentFormatters,
@@ -20,6 +23,7 @@
     App.vue.globalComponents['vue-info'] = App.vue.components.vueInfo;
     App.vue.globalComponents['vue-transition'] = App.vue.components.vueTransition;
     App.vue.globalComponents['vue-checkmark'] = App.vue.components.vueCheckmark;
+    App.vue.globalComponents['vue-input'] = App.vue.components.vueInput;
     App.vue.globalComponents['vue-code'] = App.vue.components.vueCode;
     App.vue.globalComponents['vue-fail'] = App.vue.components.vueFail;
     App.vue.globalComponents['vue-radial-progress-bar'] = App.vue.components.vueRadialProgressBar;
