1658fac3ecba431cd5c83d6e55b70c69e8094184
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-13 12:36:22 -0500

    Create main.js

---
+++ app/assets/javascripts/application/config/main.js
@@ -0,0 +1,29 @@
+(function() {
+  "use strict";
+
+  App.register('config').enter(function() {
+
+    // Displays all available UI options in the warnings
+    App.vue.config.main.showInterfaces = true;
+
+    // Interfaces included into every vue component
+    App.vue.config.main.includedInterfaces = [
+      App.vue.interfaces.contentFormatters,
+      App.vue.interfaces.string,
+      App.vue.interfaces.number,
+      App.vue.interfaces.array,
+      App.vue.interfaces.date,
+      App.vue.interfaces.custom || {}
+    ];
+
+    // Components registered globably
+    App.vue.globalComponents['vue-info'] = App.vue.components.vueInfo;
+    App.vue.globalComponents['vue-transition'] = App.vue.components.vueTransition;
+    App.vue.globalComponents['vue-checkmark'] = App.vue.components.vueCheckmark;
+    App.vue.globalComponents['vue-code'] = App.vue.components.vueCode;
+    App.vue.globalComponents['vue-fail'] = App.vue.components.vueFail;
+    App.vue.globalComponents['vue-radial-progress-bar'] = App.vue.components.vueRadialProgressBar;
+    App.vue.globalComponents['vue-slider'] = App.vue.components.vueSlider;
+    App.vue.globalComponents['vue-spinner'] = App.vue.components.vueSpinner;
+  });
+})();
