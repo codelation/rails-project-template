41d771be2dc120757d97599a00ae6678ebd0332d
Jake Humphrey <humphreyja@users.noreply.github.com>
2017-04-18 09:39:39 -0500

    Update index.js

--- app/assets/javascripts/application/views/pages/index.js
+++ app/assets/javascripts/application/views/pages/index.js
@@ -4,32 +4,18 @@
   "use strict"
 
   App.register('pages.index').enter(function() {
-    App.pagesIndexView = new Vue({
-      el: '#example-component',
-      components: {
-        'additional-component': App.additionalComponent // Store in /components
-      },
+    App.vue.root = new Vue({
+      el: '#v-page',
       data: function() {
         return {
-          selected: false,
-          message: 'hello world!',
-          list: [
-            { text: 'one' },
-            { text: 'two' },
-            { text: 'three' }
-          ]
+          
         }
       },
       methods: {
-        reverseMessage: function () {
-          this.message = this.message.split('').reverse().join('')
-        }
+        
       }
     });
   }).exit(function() {
     //When page exits
-
-    //uninitialize the vue component
-    App.pagesIndexView = null;
   });
 })();
